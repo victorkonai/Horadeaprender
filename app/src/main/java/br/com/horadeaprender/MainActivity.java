8ce3fa27f09df15ffe6e70becd78b14f61f04c05
dionleno <dhonlennon10@hotmail.com>
2019-06-16 17:30:48 -0300

    Montando menu

--- app/src/main/java/br/com/horadeaprender/MainActivity.java
+++ app/src/main/java/br/com/horadeaprender/MainActivity.java
@@ -95,17 +95,13 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_home) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_fazer_simulado) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_verificar_desempenho) {
 
-        } else if (id == R.id.nav_tools) {
+        } else if (id == R.id.nav_enviar_questao) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_sobre) {
 
         }
 
